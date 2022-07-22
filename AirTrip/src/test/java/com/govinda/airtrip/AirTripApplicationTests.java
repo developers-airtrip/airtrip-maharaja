a0f806ca772be8bd1990681d6cd493f573993344
developers-airtrip <developers.airtrip@gmail.com>
2022-07-22 22:26:13 +0530

    Adding empty project

---
+++ AirTrip/src/test/java/com/govinda/airtrip/AirTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govinda.airtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
